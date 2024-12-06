b2290b75814a90d4555e919dc46883d5e73f5d61
Abhishek <abhishekmaddheshiya632@gmail.com>
2024-12-06 20:48:15 +0530

    slider section

--- NiceAtNoon/script.js
+++ NiceAtNoon/script.js
@@ -1,22 +1,30 @@
-const h1 = document.querySelectorAll(".heading-section-left h1")
-const p = document.querySelectorAll(".heading-section-right p")
-const btn = document.querySelectorAll(".right-btn")
 
-gsap.from(h1, {
+gsap.from(".hero-section-left h1", {
     y : 200,
     opacity : 0,
     duration: 1,
     stagger : 0.1,
 })
 
-gsap.from(p, {
+gsap.from(".hero-section-right p", {
     y : 200,
     opacity : 0,
     duration: 1,
 })
 
-gsap.from(btn, {
+gsap.from(".right-btn", {
     y : 200,
     opacity : 0,
     duration: 1,
 })
+
+gsap.to(".slider-section", {
+    x: "-200vw", // Move the slider section to the left by 100%
+    scrollTrigger: {
+      trigger: ".slider-section", // The section starts the animation
+      start: "top top",           // Start when the section reaches the top
+      end: "bottom top",          // End when the section scrolls out of view
+      scrub: true,                // Smooth scrolling animation
+      pin: true,                  // Pin the slider during the animation
+    },
+  });
